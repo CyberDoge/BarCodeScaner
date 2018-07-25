6694e8944138892f37c760843a78870e743dfed9
Ganin Pavel <ganin.paul@gmail.com>
2018-07-26 01:08:26 +0400

    Sale page add

--- app/src/main/java/com/pganin/barcodescaner/SaleActivity.java
+++ app/src/main/java/com/pganin/barcodescaner/SaleActivity.java
@@ -1,11 +1,18 @@
 package com.pganin.barcodescaner;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
+
+import java.util.Collection;
+import java.util.Collections;
 
 public class SaleActivity extends AppCompatActivity {
 
@@ -22,7 +29,27 @@ public class SaleActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                LoadMainTask loadMainTask = new LoadMainTask();
+                loadMainTask.execute(0);
+
             }
         });
+    }
+    private void startMain(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+    private class LoadMainTask extends AsyncTask<Integer, Void, String> {
+        @SuppressWarnings("unchecked")
+        @Override
+        protected String doInBackground(Integer... params) {
+            try {
+                Thread.sleep(1000);
+                startMain();
+            } catch (Exception e) {
+                //Log.e(TAG, "Loading data", e);
+            }
+            return "";
+        }
     }
 }
